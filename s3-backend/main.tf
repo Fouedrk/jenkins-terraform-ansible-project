fef802b2241c37aeceb25d0f3a375a64362c1a51
fouedrk <fouedrekik97@gmail.com>
2022-12-02 11:21:03 +0100

    first commit

---
+++ s3-backend/main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias   = "va"
+  profile = "fouedrk"
+  shared_credentials_files = [" home/.aws/credentials "]
+}
+
+locals {
+  tags = {
+    project = var.project
+  }
+}
+
+data "aws_region" "current" {}
+
+resource "aws_resourcegroups_group" "resourcegroups_group" {
+  name = "${var.project}-s3-backend"
+
+  resource_query {
+    query = <<-JSON
+      {
+        "ResourceTypeFilters": [
+          "AWS::AllSupported"
+        ],
+        "TagFilters": [
+          {
+            "Key": "project",
+            "Values": ["${var.project}"]
+          }
+        ]
+      }
+    JSON
+  }
+}
+
+output "config" {
+  value = {
+    bucket         = aws_s3_bucket.s3_bucket.bucket
+    region         = data.aws_region.current.name
+    role_arn       = aws_iam_role.iam_role.arn
+    dynamodb_table = aws_dynamodb_table.dynamodb_table.name
+  }
+}
